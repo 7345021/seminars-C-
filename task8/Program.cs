6b004c8e59d36f786f8a9932c67c6c94fa5491e2
Andrey <trusov1967@gmail.com>
2022-02-03 14:33:16 +0300

    решение задачи №8

---
+++ task8/Program.cs
@@ -0,0 +1,17 @@
+﻿int[] array = { 1, 2, 46, 4, 5, 6, 7, 8, 9, 26 };
+int c;
+int n = array.Length;
+int index = 0;
+
+
+while (index <= n)
+{
+    if (array[index] % 2 == 0)
+    {
+        c = array[index];
+        Console.WriteLine(c);
+    }
+
+    index++;
+}
+
