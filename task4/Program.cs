d9fe5f65b2416decab2fc66f945969e93ef50f92
Andrey <trusov1967@gmail.com>
2022-02-03 12:07:29 +0300

    решение задачи №4

---
+++ task4/Program.cs
@@ -0,0 +1,13 @@
+﻿double A = 47;
+double B = -2.8;
+double C = 9.43;
+
+double max = A;
+
+if (A > max) max = A;
+if (B > max) max = B;
+if (C > max) max = C;
+
+Console.Write("max= ");
+Console.WriteLine(max);
+
