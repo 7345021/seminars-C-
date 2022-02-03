b491cd80f9fbd5eb1a74cb7695c454de499d6e70
Andrey <trusov1967@gmail.com>
2022-02-03 12:01:04 +0300

    решение задачи №2

---
+++ task2/Program.cs
@@ -0,0 +1,15 @@
+﻿double A = -98.7;
+double B = 9.23;
+
+double max = A;
+double min = B;
+
+if (A > B) max = A;
+if (A > B) min = B;
+if (B > A) max = B;
+if (B > A) min = A;
+Console.Write("max= ");
+Console.WriteLine(max);
+Console.Write("min= ");
+Console.WriteLine(min);
+
