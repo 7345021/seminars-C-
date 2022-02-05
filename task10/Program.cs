964c363bfe59925acc9c6ae8b0f691a0ac99faf7
Andrey <trusov1967@gmail.com>
2022-02-05 13:19:36 +0300

    решение задачи №10

---
+++ task10/Program.cs
@@ -0,0 +1,15 @@
+﻿// показать вторую цифру трехзначного числа.
+int GetNumber()//объявили функцию
+{
+    int randomNumber = new Random().Next(100, 1000);//тело
+    return randomNumber;
+}
+int number = GetNumber();//вызвали функцию
+Console.WriteLine($"сгененрированное число: {number}");
+int SecondDigit()//объявили новую функцию
+{
+    int result = (number % 100) / 10;
+    return result;
+}
+int secondNumber = SecondDigit();//вызвали функцию
+Console.WriteLine($"цифра во втором разряде: {secondNumber}");
