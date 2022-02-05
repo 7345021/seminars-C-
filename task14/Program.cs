3a3d6b0b905d019966ee3ddb976ba0efe5b56887
Andrey <trusov1967@gmail.com>
2022-02-05 22:38:46 +0300

    рещение задачи№14

---
+++ task14/Program.cs
@@ -0,0 +1,18 @@
+﻿//найти третью цифру числа или сообщить, что ее нет.
+int GetRandom()
+{
+    int digit = new Random().Next(0, 1000);
+    return digit;
+}
+int number = GetRandom();
+Console.WriteLine($"любое случайное число: {number}");
+
+int result=number / 10;
+if (result >= 10)
+{
+   Console.WriteLine($"третья цифра: {number%10}");
+}
+else 
+{
+    Console.WriteLine($"Третья цифра отсутствует");
+}
