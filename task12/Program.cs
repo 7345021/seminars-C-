a7e6c23848f0c60c480bad83c15d82376aad33fb
Andrey <trusov1967@gmail.com>
2022-02-05 16:01:09 +0300

    решение задачи №12

---
+++ task12/Program.cs
@@ -0,0 +1,13 @@
+﻿// Удалить вторую цифру трехзначного числа.
+int GetNumber() //объявили функцию
+{
+    int randomNumber=new Random().Next(100, 1000); 
+    return randomNumber;
+}
+int number=GetNumber();
+Console.WriteLine($"случайное число: {number}");
+
+int firstDigit=number/100;
+int thirdDigit=number %10;
+Console.WriteLine($"удален второй разряд: {firstDigit}_{thirdDigit}");
+
