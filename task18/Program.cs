72f19c20b9ded47193e0b9a39b2322ed4112fc75
Andrey <trusov1967@gmail.com>
2022-02-10 18:26:45 +0300

    Решение задачи №18

---
+++ task18/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 18: Проверить истинность утверждения¬(X ⋁ Y) = ¬X ⋀ ¬Y
+
+
+
+   void LogicFunction(bool a, bool b)
+   {
+    
+       Console.WriteLine($"{!(a || b)}= {!a && !b}");
+   }
+   LogicFunction(false, false);
+   LogicFunction(false, true);
+   LogicFunction(true, false);
+   LogicFunction(true, true);
+
