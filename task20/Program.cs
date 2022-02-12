52fadaacfd3fc5fe2a2229fcdbf4c047b6ec3640
Andrey <trusov1967@gmail.com>
2022-02-12 09:04:04 +0300

    Решение задачи №20

---
+++ task20/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задача 20: Задать номер четверти, показать диапазоны для возможных координат
+int InPut(string message)
+{
+    Console.WriteLine(message);
+    string num1 = Console.ReadLine();
+    return int.Parse(num1);
+}
+void NumberPart(int x, int y)
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("I Четверть: X>0, Y>0");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("II Четверть: X<0, Y>0");
+    }
+    else if (x < 0 && y < 0)
+    {
+        Console.WriteLine("III  Четверть: X<0, Y<0");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("IV Четверть: X>0, Y<0");
+    }
+}
+int x = InPut("Введите координату Х: ");
+int y = InPut("Введите координату Y: ");
+NumberPart(x, y);
+
