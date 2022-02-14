18fb5a1ff75be0aa4c2b0643db4a0f653eedb199
Andrey <trusov1967@gmail.com>
2022-02-14 18:41:00 +0300

    Решение задачи №26

---
+++ task26/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 26.Возведите число А в натуральную степень B используя цикл.
+
+int InPut(string message)
+{
+    Console.WriteLine(message);
+    string num1 = Console.ReadLine();
+    return int.Parse(num1);
+}
+int number = InPut("Введите число А (основание степени): ");
+int degree = InPut("Введите число В (показатель степени): ");
+
+double result = number;
+
+for (int i = 1; i < degree; i++)
+{
+    result *= number;
+}
+Console.Write($"{number}^{degree}={result}");
