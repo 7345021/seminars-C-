91769988806e004eaddcc3d2bbad9e72d53a1986
Andrey <trusov1967@gmail.com>
2022-02-21 18:28:30 +0300

    Решение задачи №44

---
+++ task44/Program.cs
@@ -0,0 +1,13 @@
+﻿//Задача 44. Найти точку пересечения двух прямых заданных 
+//уравнением y = k1 * x + b1, y = k2 * x + b2, 
+//b1 k1 и b2 и k2 заданы.
+double k1 = -6;
+double b1 = 9;
+double k2 = 2;
+double b2 = -3;
+if (k1 == k2)
+{
+    Console.WriteLine("Прямые не пересекаются");
+}
+double x = (b1 - b2) / (k2 - k1);
+Console.WriteLine($"Х равен {x}");
