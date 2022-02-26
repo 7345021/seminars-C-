47c12df32e41e07eb515215b67fee28c08b6892b
Andrey <trusov1967@gmail.com>
2022-02-26 23:23:52 +0300

    Решение задачи №54

---
+++ task54/Program.cs
@@ -0,0 +1,46 @@
+﻿//Задача 54: В матрице чисел найти сумму элементов главной диагонали
+var arr = new int[5, 5];
+Random randomNumber = new Random();
+void FillArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = randomNumber.Next(1, 10);
+        }
+    }
+}
+void PrintArray()
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void NumberSum()
+{
+    int result = 0;
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            if (i == j)
+            {
+                result = result + arr[i, j];
+                
+            }
+           
+
+        }
+
+    } Console.WriteLine($"Сумма чисел главной диагонали равна: {result} ");
+}
+
+FillArray();
+PrintArray();
+NumberSum();
